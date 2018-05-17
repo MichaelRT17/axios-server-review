ed3833e876b604b2cd04e4e39c9088db797b0d59
Michael Thurman <michaelrt17@icloud.com>
2018-05-16 21:12:34 -0600

    finished the review

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
 
   componentDidMount() {
     axios.get('/api/getFriends').then(res => {
+      console.log(res.data)
       this.setState({ friends: res.data })
     })
   }
